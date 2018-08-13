f27bc0ff9e055e8d8900c699a003888f8ec26b2a
Noah Bloom <ntbloom@gmail.com>
2018-08-12 22:27:56 -0400

    layout complete, need to add code

--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 from flask import Flask, render_template, request, redirect
 import requests, codecs 
 
+app = Flask(__name__)
+
+'''creating the book dictionary'''
 # connect to the book
 book = requests.get('http://www.gutenberg.org/cache/epub/2489/pg2489.txt')
 book.raise_for_status()
-
 # loop through and count paragraphs at are longer than a single line
 i = 0
 bookDict = {} # dictionary for storing entire book
 candidates = []
 for chunk in book.iter_content(100000):
-    text = codecs.decode(chunk, 'unicode_escape') # decode the weirdly coded text chars (\\n, \\r) 
+    text = codecs.decode(chunk, 'unicode_escape') # decode the weirdly coded text chars
     paragraphs = text.split("\n\r\n") # split on paragraph seperator
     for p in paragraphs:
         bookDict[i+1] = p
@@ -21,8 +23,7 @@ for chunk in book.iter_content(100000):
         if '\n' in p: # if it's more than a single line, like a chapter heading
             candidates += [i]
 
-a = int(input('#: \n'))
-print(bookDict[a])
-while a != 0:
-    a = int(input('#: \n'))
-    print(bookDict[a])
+@app.route('/')
+def index():
+    # TODO: enter code for entering text into html table
+    return render_template('index.html', number = number, text = text)

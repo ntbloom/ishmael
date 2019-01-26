5c48b24c1ab88e8b2d577ad4dc35222f57de6f88
ntbloom <ntbloom@gmail.com>
2019-01-26 14:48:01 -0500

    Randpop ready for deployment

--- noRepeats/Randpop.py
+++ noRepeats/Randpop.py
@@ -1,5 +1,9 @@
-# noRepeats.py
-# makes funBook not repeat any paragraphs in its script
+# Randpop.py
+# 
+# mimicks a pop/push stack in sqlite for pulling random numbers and
+# recording each iteration
+#
+# used to make funBook not repeat any paragraphs in its script
 
 #!usr/bin/python3
 import sqlite3, datetime
@@ -7,7 +11,7 @@ from createDB import createDB
 
 
 class Randpop:
-    def __init__(self, database='./paragraphs.db', first=63, last=70):
+    def __init__(self, database='./paragraphs.db', first=63, last=2964):
         self.database = database
         self.first = first  # paragraph number to start
         self.last = last    # paragraph number to finish 
@@ -16,35 +20,33 @@ class Randpop:
 
     def pullNum(self):
         '''pops random number from master, adds to used'''
-        try:    # until all paragraphs run out
-            
-            # pull random number from master
-            self.cursor.execute('SELECT * from master ORDER BY RANDOM() LIMIT 1') 
-            rand = self.cursor.fetchall() 
-           
-           
-            rand = [i[0]for i in rand]
-            print('rand: ', rand)
+        # checks master to see if there are still numbers available
+        self.cursor.execute('SELECT * from master')
+        test = self.cursor.fetchall()
         
-            # pops from 'master', pushes to 'used' with date stamp
-            date = datetime.datetime.now() 
-            self.cursor.execute('INSERT INTO used (id, day, month, year) \
-                VALUES (?,?,?,?)', [rand[0], date.day, date.month, date.year])
-            self.cursor.execute('DELETE FROM master WHERE id=?', (rand[0],))
-            # commit changes
-            self.conn.commit()
+        # pull random number from master
+        self.cursor.execute('SELECT * from master ORDER BY RANDOM() LIMIT 1') 
+        rand = self.cursor.fetchall()
+        rand = [i[0]for i in rand] # converts from tuple to list
+    
+        # pops from 'master', pushes to 'used' with date stamp
+        date = datetime.datetime.now() 
+        self.cursor.execute('INSERT INTO used (id, day, month, year) \
+            VALUES (?,?,?,?)', [rand[0], date.day, date.month, date.year])
+        self.cursor.execute('DELETE FROM master WHERE id=?', (rand[0],))
+        
+        # commit changes
+        self.conn.commit()
 
-        except:
-            # start all over again
+        # restarts database if it runs out of numbers
+        if len(test) == 1:
             self.createDB()
-            self.pullNum()
-
+            #TODO: add anything else you want to happen when book ends
     
     def createDB(self):
-        '''creates database with list according to Moby Dick paragraphs'''
+        '''creates database with list starting at first, ending at last'''
         
         # create tables
-        
         self.cursor.execute('DROP TABLE IF EXISTS master')
         self.cursor.execute('CREATE TABLE master (id integer)')
         self.cursor.execute('DROP TABLE IF EXISTS used')
@@ -52,16 +54,15 @@ class Randpop:
             (id integer, month integer, day integer, year integer)')
 
         # define upper and lower limits for paragraphs, dump into a list
-        
         array = []
-        for i in range(self.last-self.first+1):
-            i = self.first
+        first = self.first
+        last = self.last
+        for i in range(last-first+1):
+            i = first
             array.append(i)
-            self.first += 1
-
+            first += 1
 
         # populates master table
-
         self.cursor.execute('DELETE FROM master')
         self.cursor.execute('DELETE from used')
         for i in range(len(array)):
@@ -73,7 +74,6 @@ class Randpop:
         self.cursor.execute('SELECT * from used')
         used = self.cursor.fetchall()
         print('used:\n', used)
-        
         self.cursor.execute('SELECT * from master')
         master = self.cursor.fetchall()
         master = [i[0] for i in master]
